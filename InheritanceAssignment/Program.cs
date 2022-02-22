2a206faebd392193fc8961008058c9a014b662cd
USER <USER@DESKTOP-0N68O5J>
2022-02-22 10:44:03 +0100

    Add project files.

---
+++ InheritanceAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace InheritanceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Telephone telephone1 = new DigitalPhone();
+            Telephone telephone2 = new TalkingPhone();
+
+            telephone1.Ring();
+            telephone2.Ring();
+        } 
+    }
+}
---
+++ InheritanceAssignment/Telephone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAssignment
+{
+    public abstract class Telephone
+    {
+        protected string phonetype;
+
+        public abstract void Ring();
+        
+    }
+
+    public class DigitalPhone : Telephone
+    {
+        public DigitalPhone()
+        {
+            this.phonetype = "Digital";
+        }
+        public override void Ring()
+        {
+            Console.WriteLine("{0} phone ringing..... Baaaa Boooooo", phonetype);
+        }
+    }
+    public class TalkingPhone : Telephone
+    {
+        public TalkingPhone()
+        {
+            this.phonetype = "Talking";
+        }
+        public override void Ring()
+        {
+            Console.WriteLine("{0} phone ringing... zuuu zuuuu", phonetype);
+            
+        }
+    }
+        
+
+    
+
+    
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
